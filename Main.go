f31546b5cc45860a41d2d11dff96f07cf4dd87fc
Gleb Danichev <glebio@yandex-team.ru>
2019-09-04 16:43:09 +0300

    Calc lines

--- Main.go
+++ Main.go
@@ -1,8 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello")
+	rootDir := "/Users/glebio/go/src/gitMetrics"
+	result := map[string]uint64{}
+	sloc(rootDir, &result)
+	fmt.Println(result)
+}
 
+func sloc(dirname string, result *map[string]uint64) {
+	file, err := os.Open(dirname)
+	if err != nil {
+		log.Println("cant open " + err.Error() + " " + dirname)
+		return
+	}
+	infos, err := file.Readdir(0)
+	if err != nil {
+		log.Println("cant open " + err.Error() + " " + dirname)
+		return
+	}
+
+	for _, f := range infos {
+		fName := dirname + "/" + f.Name()
+		if f.IsDir() {
+			sloc(fName, result)
+		} else {
+			regFile, err := os.Open(fName)
+			if err != nil {
+				log.Println("cant open " + err.Error() + " " + fName)
+				continue
+			}
+			counter, err := lineCounter(regFile)
+			if err != nil {
+				log.Println("cant lineCounter " + err.Error() + " " + fName)
+				continue
+			}
+			(*result)["13"] += uint64(counter)
+		}
+	}
+
+}
+
+func lineCounter(r io.Reader) (int, error) {
+	buf := make([]byte, 32*1024)
+	count := 0
+	lineSep := []byte{'\n'}
+
+	for {
+		c, err := r.Read(buf)
+		count += bytes.Count(buf[:c], lineSep)
+
+		switch {
+		case err == io.EOF:
+			return count, nil
+
+		case err != nil:
+			return count, err
+		}
+	}
 }
