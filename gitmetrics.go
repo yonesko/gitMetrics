02f860bcf4346f38f70235e2448eea4f3664d07e
Gleb Danichev <glebio@yandex-team.ru>
2019-09-06 11:46:36 +0300

    sort.SortByValue

--- gitmetrics.go
+++ gitmetrics.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-	"gitmetrics/utils"
+	"gitmetrics/sort"
 	"io"
 	"log"
 	"math"
@@ -31,7 +31,7 @@ func main() {
 func printReport(result map[string]uint64, started time.Time) {
 	fmt.Printf("Elapsed %v\n", time.Since(started))
 	fmt.Println("Lines of code by extension:")
-	pairs := utils.SortByValue(result)
+	pairs := sort.SortByValue(result)
 	for _, pair := range pairs[:uint64(math.Min(float64(15), float64(len(pairs))))] {
 		fmt.Printf("%v %v\n", pair.Key, pair.Val)
 	}
