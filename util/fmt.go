4d4599d802c5d0617a69dc32516849d41eff5ee9
Глеб Даничев <g.danichev@city-mobil.ru>
2020-02-06 17:24:54 +0300

    AbrvInteger

--- util/fmt.go
+++ util/fmt.go
@@ -5,11 +5,12 @@ import (
 )
 
 func AbrvInteger(v uint64) string {
+	f := float64(v)
 	if v >= 1e6 {
-		return fmt.Sprintf("%v%v", v/1e6, "m")
+		return fmt.Sprintf("%.1fm", f/1e6)
 	}
 	if v >= 1e3 {
-		return fmt.Sprintf("%v%v", v/1e3, "k")
+		return fmt.Sprintf("%.1fk", f/1e3)
 	}
 
 	return fmt.Sprint(v)
